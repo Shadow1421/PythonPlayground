834c021f4c7696aec611c6e5d707f1c02c46d86a
neeraj-singh <99228532+zero2one-developer@users.noreply.github.com>
2023-07-01 13:08:08 +0530

    convert all code into function block

--- readTextFileUpdatedLogic.py
+++ readTextFileUpdatedLogic.py
@@ -1,48 +1,66 @@
 import os
 
-# Check if the file exists
-if os.path.isfile("employee.txt"):
+def read_employee_data():
+    # Check if the file exists
+    if os.path.isfile("employee.txt"):
+        # Open the file in read mode
+        with open("employee.txt", "r") as file:
+            # Read the contents of the file
+            contents = file.read()
+
+            # Display the contents
+            print("Existing Employee Data:")
+            print(contents)
+
+def add_employee_data():
+    # Prompt the user for inputs
+    name = input("Enter employee name: ")
+    department = input("Enter department: ")
+    salary = input("Enter salary: ")
+
+    # Open the file in append mode to add new data
+    with open("employee.txt", "a") as file:
+        # Write the user inputs to the file
+        file.write(f"\n{name}, {department}, {salary}")
+
+def display_employee_data():
     # Open the file in read mode
     with open("employee.txt", "r") as file:
         # Read the contents of the file
         contents = file.read()
-        
-        # Display the contents
-        print("Existing Employee Data:")
+
+        # Display the complete data from the file
+        print("\nUpdated Employee Data:")
         print(contents)
 
-# Prompt the user for inputs
-name = input("Enter employee name: ")
-department = input("Enter department: ")
-salary = input("Enter salary: ")
-
-# Open the file in append mode to add new data
-with open("employee.txt", "a") as file:
-    # Write the user inputs to the file
-    file.write(f"\n{name}, {department}, {salary}")
-
-# Open the file again to read the updated contents
-with open("employee.txt", "r") as file:
+def search_employee_data():
     # Read the contents of the file
-    contents = file.read()
+    with open("employee.txt", "r") as file:
+        contents = file.read()
 
-# Display the complete data from the file
-print("\nUpdated Employee Data:")
-print(contents)
+    # Prompt the user to enter a name for searching
+    search_name = input("\nEnter the employee name to search: ")
 
-# Search for a user's data
-search_name = input("\nEnter the employee name to search: ")
+    # Check if the user's data exists in the file
+    if search_name in contents:
+        print(f"\nFound Employee Data for {search_name}:")
+        
+        # Split the contents into individual lines
+        lines = contents.split("\n")
+        
+        # Iterate over the lines to find the matching data
+        for line in lines:
+            if search_name in line:
+                print(line)
+    else:
+        print(f"\nEmployee Data for {search_name} not found.")
 
-# Check if the user's data exists in the file
-if search_name in contents:
-    print(f"\nFound Employee Data for {search_name}:")
-    
-    # Split the contents into individual lines
-    lines = contents.split("\n")
-    
-    # Iterate over the lines to find the matching data
-    for line in lines:
-        if search_name in line:
-            print(line)
-else:
-    print(f"\nEmployee Data for {search_name} not found.")
+# Main program flow
+def main():
+    read_employee_data()
+    add_employee_data()
+    display_employee_data()
+    search_employee_data()
+
+# Call the main function to start the program
+main()
